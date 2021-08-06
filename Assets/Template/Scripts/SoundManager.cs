cdce01fc54674d3077564f59917922df32adfa9e
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-08-06 18:58:39 +0900

    SoundManagerを修正した

--- Assets/Template/Scripts/SoundManager.cs
+++ Assets/Template/Scripts/SoundManager.cs
@@ -35,11 +35,24 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
     [SerializeField] AudioSource m_seAudioSource = null;
     [Header("VOICEのAudioSource")]
     [SerializeField] AudioSource m_voiceAudioSource = null;
+    [Header("デバッグ用")]
     [SerializeField] bool m_debug = false;
+    /// <summary> マスター音量時のフラグ </summary>
+    bool masterVolumeChange = false;
+    /// <summary> BGM音量時のフラグ </summary>
+    bool bgmVolumeChange = false;
+    /// <summary> SE音量時のフラグ </summary>
+    bool seVolumeChange = false;
+    /// <summary> ボイス音量時のフラグ </summary>
+    bool voiceVolumeChange = false;
     Dictionary<string, int> bgmIndex = new Dictionary<string, int>();
     Dictionary<string, int> seIndex = new Dictionary<string, int>();
     Dictionary<string, int> voiceIndex = new Dictionary<string, int>();
-    
+
+    public float GetMasterVolume { get => m_masterVolume; }
+    public float GetBgmVolume { get => m_bgmVolume; }
+    public float GetSeVolume { get => m_seVolume; }
+    public float GetVoiceVolume { get => m_voiceVolume; }
 
     void Awake()
     {
@@ -116,31 +129,151 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
 
     void Update()
     {
-        m_bgmAudioSource.volume = m_bgmVolume * m_masterVolume;
-        m_seAudioSource.volume = m_seVolume * m_masterVolume;
-        m_voiceAudioSource.volume = m_voiceVolume * m_masterVolume;
+        VolumeChanger();
     }
 
+    /// <summary>
+    /// 指定したBGMを再生する
+    /// </summary>
+    /// <param name="name"> BGMの名前 </param>
     public void PlayBgmByName(string name)
     {
         PlayBgm(GetBgmIndex(name));
     }
 
+    /// <summary>
+    /// 指定したBGMを再生する
+    /// </summary>
+    /// <param name="name"> BGMの名前 </param>
     public void PlaySeByName(string name)
     {
         PlaySe(GetSeIndex(name));
     }
 
+    /// <summary>
+    /// 指定したBGMを再生する
+    /// </summary>
+    /// <param name="name"> BGMの名前 </param>
+    public void PlayVoiceByName(string name)
+    {
+        PlayVoice(GetVoiceIndex(name));
+    }
+
+    /// <summary>
+    /// BGMの再生を停止する
+    /// </summary>
     public void StopBgm()
     {
         m_bgmAudioSource.Stop();
         m_bgmAudioSource.clip = null;
     }
 
+    /// <summary>
+    /// SEの再生を停止する
+    /// </summary>
     public void StopSe()
     {
         m_seAudioSource.Stop();
         m_seAudioSource.clip = null;
+    }
+
+    /// <summary>
+    /// ボイスの再生を停止する
+    /// </summary>
+    public void StopVoice()
+    {
+        m_voiceAudioSource.Stop();
+        m_voiceAudioSource.clip = null;
+    }
+
+    /// <summary>
+    /// 各音量を変更する
+    /// </summary>
+    public void VolumeChanger()
+    {
+        if (m_bgmAudioSource && bgmVolumeChange)
+        {
+            m_bgmAudioSource.volume = m_bgmVolume * m_masterVolume;
+            bgmVolumeChange = false;
+        }
+
+        if (m_seAudioSource && seVolumeChange)
+        {
+            m_seAudioSource.volume = m_seVolume * m_masterVolume;
+            seVolumeChange = false;
+        }
+
+        if (m_voiceAudioSource && voiceVolumeChange)
+        {
+            m_voiceAudioSource.volume = m_voiceVolume * m_masterVolume;
+            voiceVolumeChange = false;
+        }
+    }
+    
+    /// <summary>
+    /// マスター音量を変更する
+    /// </summary>
+    /// <param name="masterValue"> 音量 </param>
+    public void MasterVolChange(float masterValue)
+    {
+        m_masterVolume = masterValue;
+    }
+
+    /// <summary>
+    /// BGM音量を変更する
+    /// </summary>
+    /// <param name="masterValue"> 音量 </param>
+    public void BgmVolChange(float bgmValue)
+    {
+        m_bgmVolume = bgmValue;
+        bgmVolumeChange = true;
+    }
+
+    /// <summary>
+    /// SE音量を変更する
+    /// </summary>
+    /// <param name="masterValue"> 音量 </param>
+    public void SeVolChange(float seValue)
+    {
+        m_seVolume = seValue;
+        seVolumeChange = true;
+    }
+
+    /// <summary>
+    /// ボイス音量を変更する
+    /// </summary>
+    /// <param name="masterValue"> 音量 </param>
+    public void VoiceVolChange(float voiceValue)
+    {
+        m_voiceVolume = voiceValue;
+        voiceVolumeChange = true;
+    }
+
+    void PlayBgm(int index)
+    {
+        if (Instance != null)
+        {
+            index = Mathf.Clamp(index, 0, m_bgms.Length);
+
+            m_bgmAudioSource.clip = m_bgms[index];
+            m_bgmAudioSource.loop = true;
+            m_bgmAudioSource.volume = m_bgmVolume * m_masterVolume;
+            m_bgmAudioSource.Play();
+        }
+    }
+
+    void PlaySe(int index)
+    {
+        index = Mathf.Clamp(index, 0, m_ses.Length);
+
+        m_seAudioSource.PlayOneShot(m_ses[index], m_seVolume * m_masterVolume);
+    }
+
+    void PlayVoice(int index)
+    {
+        index = Mathf.Clamp(index, 0, m_voices.Length);
+
+        m_voiceAudioSource.PlayOneShot(m_voices[index], m_voiceVolume * m_masterVolume);
     }
 
     int GetBgmIndex(string name)
@@ -167,41 +300,15 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
         }
     }
 
-    void PlayBgm(int index)
+    int GetVoiceIndex(string name)
     {
-        if (Instance != null)
+        if (voiceIndex.ContainsKey(name))
         {
-            index = Mathf.Clamp(index, 0, m_bgms.Length);
-
-            m_bgmAudioSource.clip = m_bgms[index];
-            m_bgmAudioSource.loop = true;
-            m_bgmAudioSource.volume = m_bgmVolume * m_masterVolume;
-            m_bgmAudioSource.Play();
+            return voiceIndex[name];
         }
-    }
-
-    void PlaySe(int index)
-    {
-        index = Mathf.Clamp(index, 0, m_ses.Length);
-
-        m_seAudioSource.PlayOneShot(m_ses[index], m_seVolume * m_masterVolume);
-    }
-
-    
-
-    public void MasterVolChange()
-    {
-        m_masterVolume = GameObject.FindGameObjectWithTag("Master").GetComponent<Slider>().value;
-        Debug.Log(m_masterVolume);
-    }
-    public void BGMVolChange()
-    {
-        m_bgmVolume = GameObject.FindGameObjectWithTag("BGM").GetComponent<Slider>().value;
-        Debug.Log(m_bgmVolume);
-    }
-    public void SEVolChange()
-    {
-        m_seVolume = GameObject.FindGameObjectWithTag("SE").GetComponent<Slider>().value;
-        Debug.Log(m_seVolume);
+        else
+        {
+            return 0;
+        }
     }
 }
